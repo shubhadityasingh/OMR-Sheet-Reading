7c95a20da9c18d6c5f18678ef8c2b0921b86fe57
unknown <shubh28062000@gmail.com>
2025-04-08 11:57:41 +0530

    logging statements added

--- server.py
+++ server.py
@@ -26,11 +26,13 @@ def executeOMRReadingCode():
     FTP_Student_ID = request.args.get('FTP_Student_ID')
     if FTP_Student_ID == None:
         FTP_Student_ID = ''
+    print(">> 01 -- Request parameters parsed <<")
     status, message = main_code.everything(FTP_Path, FTP_Paper_ID, FTP_Student_ID)
+    print(">> 02 -- OMR processing completed <<")
     if status == 1:
         status, message = exportCSV.exportCSVFile(FTP_Path, FTP_Paper_ID, FTP_Student_ID)
-    
-    print(status, message)
+    print(">> 03 -- CSV export completed <<")
+    print(">> 04 -- ", status, " -- ", message, " <<")
     return jsonify(status=status, message=message)
     # return jsonify(FTP_Path = FTP_Path, FTP_Paper_ID = FTP_Paper_ID, FTP_Student_ID = FTP_Student_ID)
 
